6963a69c54b2bcf656357c59e1144f34f92e3b44
rakinu21 <rakinkinra@gmail.com>
2025-01-25 21:03:00 +0800

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -3,8 +3,20 @@ import dotenv from 'dotenv'
 import { connectDb } from './config/db.js';
 import authRouter from './routes/authRouter.js';
 import cookieParser from 'cookie-parser';
+import path from 'path'
+
+
+const __dirname = path.resolve();
+
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req ,res) => {
+    res.sendFile(path.join(__dirname, 'client' , 'dist', 'index.html'))
+})
+
 dotenv.config();
 app.use(express.json())
 app.use(cookieParser());
@@ -19,3 +31,4 @@ app.listen(process.env.PORT || 4000, () => {
     connectDb()
     console.log('server running', process.env.PORT)
 })
+
