2955f554f1bdf1e31ec1b9825f77075c27626840
rakinu21 <rakinkinra@gmail.com>
2025-01-25 23:55:43 +0800

    add changes db.js

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO);
+        const conn = await mongoose.connect(process.env.MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`connected to the mongoDB || ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
